f132c815e4184efa7c67fe1589be2eec2a28aeb6
Lintao <Lintao.Amons@outlook.com>
2023-06-11 14:32:56 +0800

    Add get cmd

---
+++ cmd/get.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get an account by account name",
+	Run: func(cmd *cobra.Command, args []string) {
+		var name string
+		if len(args) > 0 {
+			name = args[0]
+		} else {
+      name, _ = pterm.DefaultInteractiveTextInput.WithMultiLine(false).Show("Name")
+		}
+    // TODO: use UsecaseFactory
+
+		fmt.Println(name)
+	},
+}
+
+func init() {
+	accountCmd.AddCommand(getCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
